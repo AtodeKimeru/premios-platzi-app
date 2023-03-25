60643c2ab08c85a9aa84e8c8caf318d78a47976d
IvanC <ibarragan@unal.edu.co>
2023-03-25 01:42:12 -0500

    bug in model Question and in view polls:index and QuestionIndexViewTest added with 2 tests

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls.base import reverse
 
 from .models import Question
 
@@ -12,7 +13,7 @@ class QuestionModelTest(TestCase):
     def test_was_published_recently_with_future_questions(self):
         """was_published_recently returns False for questions whose pub_date is in the future"""
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text="¿Quién es el mejor Course Director de Platzi?", 
+        future_question = Question(question_text='¿Quién es el mejor Course Director de Platzi?', 
                                    pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
@@ -20,7 +21,7 @@ class QuestionModelTest(TestCase):
     def test_was_published_recently_with_past_questions(self):
         """was_published_recently returns False for questions whose pub_date is greater than 24 hours old"""
         time = timezone.now() - datetime.timedelta(days=3)
-        past_question = Question(question_text="¿Quién es el mejor Course Director de Platzi?", 
+        past_question = Question(question_text='¿Quién es el mejor Course Director de Platzi?', 
                                    pub_date=time)
         self.assertIs(past_question.was_published_recently(), False)
 
@@ -28,6 +29,28 @@ class QuestionModelTest(TestCase):
     def test_was_published_recently_with_present_questions(self):
         """was_published_recently returns True for questions whose pub_date is less than 24 hours old"""
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-        present_question = Question(question_text="¿Quién es el mejor Course Director de Platzi?", 
+        present_question = Question(question_text='¿Quién es el mejor Course Director de Platzi?', 
                                    pub_date=time)
         self.assertIs(present_question.was_published_recently(), True)
+
+
+class QuestionIndexViewTest(TestCase):
+    
+    def test_no_questions(self):
+        """If no question exist, an appropiate message is displayed"""
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No polls are available.')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+
+    def test_future_questions(self):
+        """the future questions are not visible"""
+        time = timezone.now() + datetime.timedelta(days=365)
+        future_question = Question(question_text='¿Quién es el mejor Course Director de Platzi?', 
+                                   pub_date=time)
+        future_question.save()
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No polls are available.')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
